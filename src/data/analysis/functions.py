3628c5d7c91916d6f25c9d42a44b9dbfdc8e2212
Juan <juanmariaroldanr@gmail.com>
2024-07-29 13:59:03 +0000

    added relative success function

--- src/data/analysis/functions.py
+++ src/data/analysis/functions.py
@@ -1,5 +1,6 @@
 import pyspark.sql.functions as F
 
+
 def discrepancifier(df):
     """
     detect discrepancies per row where there are the four
@@ -117,3 +118,39 @@ def discrepancifier(df):
             .otherwise(F.lit("dispar")),
         ),
     )
+
+
+def relative_success(array1):
+    from scipy.stats.contingency import relative_risk
+
+    ### take numbers from array
+    a, b = array1[0]
+    c, d = array1[1]
+    ####
+    """
+    Where zeros cause problems with computation of the relative risk or its standard error,
+    0.5 is added to all cells (a, b, c, d) (Pagano & Gauvreau, 2000; Deeks & Higgins, 2010).
+    """
+    total_expo = a + b
+    total_noExpo = c + d
+    ### for cases when total_expo/total_noExpo = 0,
+    ### we sum 1 to avoid errors an get at least 0 in the %
+    if any(t == 0 for t in [total_expo, total_noExpo]):
+        total_expo = total_expo + 1
+        total_noExpo = total_noExpo + 1
+        ### calculate relative success
+        relative_success = relative_risk(a, total_expo, c, total_noExpo)
+        ### calculate confidence intervals
+        rs_ci = relative_risk(a, total_expo, c, total_noExpo).confidence_interval(
+            confidence_level=0.95
+        )
+    else:
+
+        ### calculate relative success
+        relative_success = relative_risk(a, total_expo, c, total_noExpo)
+        ### calculate confidence intervals
+        rs_ci = relative_risk(a, total_expo, c, total_noExpo).confidence_interval(
+            confidence_level=0.95
+        )
+
+    return relative_success.relative_risk, rs_ci
