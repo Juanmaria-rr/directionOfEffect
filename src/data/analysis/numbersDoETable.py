2014888a086c9a862e91c6161c158f21c81c621d
juanmaria-rr <juanr@inst-builder-debian-12-build-build-tm6hg.us-central1-a.c.gce-image-builder.internal>
2025-02-10 13:49:18 +0000

    adapted to new coloc

--- src/data/analysis/numbersDoETable.py
+++ src/data/analysis/numbersDoETable.py
@@ -1,32 +1,14 @@
 """ Build datasets with DoE evidence and assoc numbers"""
 
-from DoEAssessment import directionOfEffect
+from DoEAssessment import directionOfEffect, temporary_directionOfEffect,build_gwasResolvedColoc
 from pyspark.sql import SparkSession
-import pyspark.sql.functions as F
+import pyspark.sql.functions as F, Window 
 from pyspark.ml.feature import QuantileDiscretizer
 
 spark = SparkSession.builder.getOrCreate()
 
-platform_v = "24.06"
-
-doe_sources = [
-    "ot_genetics_portal",
-    "gene_burden",
-    "eva",
-    "eva_somatic",
-    "gene2phenotype",
-    "orphanet",
-    "cancer_gene_census",
-    "intogen",
-    "impc",
-    "chembl",
-]
-
-evidences = spark.read.parquet(
-    f"gs://open-targets-data-releases/{platform_v}/output/etl/parquet/evidence"
-)
-### take only the ones with datasources for DoE
-evidences = evidences.filter(F.col("datasourceId").isin(doe_sources))
+#platform_v = "24.12"
+path = "gs://open-targets-pre-data-releases/24.12-uo_test-3/output/etl/parquet/"
 
 replacement_dict = {
     "gene_burden": "GeneBurden",
@@ -39,17 +21,88 @@ replacement_dict = {
     "eva_somatic": "EvaSomatic",
     "ot_genetics_portal": "OtGenetics",
     "impc": "IMPC",
+    "gwas_credible_set" :"credibleOtGenetics"   
 }
 
+doe_sources = [
+    #"ot_genetics_portal",
+    "gene_burden",
+    "eva",
+    "eva_somatic",
+    "gene2phenotype",
+    "orphanet",
+    "cancer_gene_census",
+    "intogen",
+    "impc",
+    "chembl",
+]
 
-def datasets_numbers(evidences, platform_v, replacement_dict):
-    #### include nice name
-    prueba_assessment = (
-        directionOfEffect(evidences, platform_v)
+evidences = spark.read.parquet(
+    f"{path}/evidence"
+)
+### take only the ones with datasources for DoE
+evidences = evidences.filter(F.col("datasourceId").isin(doe_sources))
+
+
+prueba_assessment = (
+        temporary_directionOfEffect(evidences, doe_sources)
         .withColumn("datasourceAll", F.lit("All"))
-        .withColumn("niceName", F.col("datasourceId"))
-        .replace(replacement_dict, subset=["niceName"])
     )
+
+gwasResolvedColoc = build_gwasResolvedColoc(path)
+
+#### take the direction from the lowest p value
+window_spec = Window.partitionBy("targetId", "diseaseId").orderBy(
+    F.col("pValueExponent").asc()
+)
+gwasCredibleAssoc = (
+    gwasResolvedColoc.withColumn(
+        "homogenized", F.first("colocDoE", ignorenulls=True).over(window_spec)
+    )  ## added 30.01.2025
+    .select("targetId", "diseaseId", "homogenized")
+    .withColumn(
+        "homogenized",
+        F.when(F.col("homogenized").isNull(), F.lit("noEvaluable")).otherwise(
+            F.col("homogenized")
+        ),
+    )
+)
+
+### replace     
+assessment = prueba_assessment.unionByName(
+    gwasCredibleAssoc.withColumn("datasourceId", F.lit("gwas_credible_set")),
+    allowMissingColumns=True,
+).withColumn("niceName", F.col("datasourceId")).replace(replacement_dict, subset=["niceName"])
+
+
+
+def datasets_numbers_trait(prueba_assessment, buckets_number):
+    """This function creates in a long format (suitable for R) the N of evidences and association per
+    DoE section. At the end, it creates a column with the corresponding deciles of the numbers to plot their intesntiy
+    The deciles are trained using the assoc to avoid underrating numbers from the assoc respecting the evidences
+    """
+    """ 
+        usage of temporary_directionOfEffect:
+        path = "gs://open-targets-pre-data-releases/24.12-uo_test-3/output/etl/parquet/"
+
+        datasource_filter = [
+            "ot_genetics_portal",
+            "gwas_credible_sets",
+            "gene_burden",
+            "eva",
+            "eva_somatic",
+            "gene2phenotype",
+            "orphanet",
+            "cancer_gene_census",
+            "intogen",
+            "impc",
+            "chembl",
+        ]
+
+        assessment, evidences, actionType, oncolabel = temporary_directionOfEffect(
+            path, datasource_filter
+        )
+    """
 
     ## direction on trait
     unpivot_trait = "stack(2, 'protect', protect, 'risk', risk) as (type, count)"
@@ -111,19 +164,13 @@ def datasets_numbers(evidences, platform_v, replacement_dict):
         .fillna(0)
     ).withColumn("facet", F.lit("whole"))
 
-    ### discretizer function
     qds = QuantileDiscretizer(
-        numBuckets=10,
+        numBuckets=buckets_number,
         inputCol="count",
         outputCol="deciles",
     )
 
-    #### apply one by one the discretizer function
-    trait_qt = qds.fit(trait).transform(trait)
-    function_qt = qds.fit(function).transform(function)
-    whole_qt = qds.fit(whole).transform(whole)
-
-    ### union of dfs
-    result = trait_qt.union(function_qt).union(whole_qt).persist()
-
-    return result
+    all = function.union(trait).union(whole)
+    result = qds.fit(whole).transform(all)  ### train qds in Whole and transform all
+    
+    return result.toPandas().to_csv("gs://ot-team/jroldan/analysis/numbersDoeTable.csv")
